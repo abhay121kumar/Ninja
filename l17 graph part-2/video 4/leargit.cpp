69cecdffa3abdcbfde2a4b54af5556f4e8550c0d
abhaykumar <abhayk74966@gmail.com>
2023-09-06 13:18:20 +0530

    add git file

---
+++ l17 graph part-2/video 4/leargit.cpp
@@ -0,0 +1,12 @@
+when new repository creating then use this command
+git init // for initialize repository only 
+
+git add .//. for add all file
+or 
+git add all
+or
+git add (file_path) //to add specific file
+
+git commit -m "message" //
+
+git push // agar push kar rahya ho to same directory me hona chahiye
